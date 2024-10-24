85331c116f075747944c95d9adaa41ca98104bed
SuryoS911 <suryo237658@student.smkn1kandeman.sch.id>
2024-10-24 14:53:45 +0700

    alhamdulillah

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function main() {
-  
-
-
+    let hasilDadu = [1, 5, 2, 6, 2, 3]
+    let kemunculanGanjil = 0
+    let kemunculanGenap = 0
+    for (let i = 0; i < 6; i++) {
+        if (hasilDadu[i] % 2 == 0) {
+            kemunculanGenap += 1
+        } else if (hasilDadu[i] % 2 != 0) {
+            kemunculanGanjil += 1
+        }
+    }
+    console.log(`Raka mendapatkan angka ganjil sebanyak ${kemunculanGanjil} kali dan angka genap sebanyak ${kemunculanGenap} kali`)
 }
 
 module.exports = main
